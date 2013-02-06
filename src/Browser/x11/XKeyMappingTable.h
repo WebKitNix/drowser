7fcb86d5b9240dcc5efc8103164ec96cbe0aabb7
Hugo Parente Lima <hugo.lima@openbossa.org>
2013-02-06 17:09:37 -0300

    Added license header to all files.
    Reviewed by: Trust me

--- src/Browser/x11/XKeyMappingTable.h
+++ src/Browser/x11/XKeyMappingTable.h
@@ -5,7 +5,6 @@
 **
 ** This file is part of the plugins of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:LGPL$
 ** GNU Lesser General Public License Usage
 ** This file may be used under the terms of the GNU Lesser General Public
 ** License version 2.1 as published by the Free Software Foundation and
@@ -29,13 +28,6 @@
 ** Other Usage
 ** Alternatively, this file may be used in accordance with the terms and
 ** conditions contained in a signed written agreement between you and Nokia.
-**
-**
-**
-**
-**
-**
-** $QT_END_LICENSE$
 **
 ****************************************************************************/
 

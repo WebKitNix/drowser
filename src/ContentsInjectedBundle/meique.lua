64768b8a2d13193c52a72643194045fe824dc34f
Hugo Parente Lima <hugo.lima@openbossa.org>
2013-03-07 21:38:51 -0300

    Removed workaround for bug already fixed on meique.
    Reviewed by: Trust me.

--- src/ContentsInjectedBundle/meique.lua
+++ src/ContentsInjectedBundle/meique.lua
@@ -6,14 +6,6 @@ pageBundle:usePackage(nix)
 pageBundle:useTarget(audio)
 pageBundle:useTarget(gamepad)
 
--- meique bug, it doesn't remember the linker libraries of static libraries
-pageBundle:usePackage(gio)
-pageBundle:usePackage(udev)
-pageBundle:usePackage(gstreamer)
-pageBundle:usePackage(gstreamerApp)
-pageBundle:usePackage(gstreamerAudio)
-pageBundle:usePackage(gstreamerFft)
-
 pageBundle:addFiles([[
     PageBundle.cpp
     PlatformClient.cpp

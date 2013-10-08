8393a45fbed69cbe75a6545d304d4f3e0f06f983
Hugo Parente Lima <hugo.lima@openbossa.org>
2013-10-08 16:36:24 -0300

    Use gstreamer 1.0 on meique based builds.
    Reviewed by: Trust me.

--- src/ContentsInjectedBundle/audio/meique.lua
+++ src/ContentsInjectedBundle/audio/meique.lua
@@ -1,7 +1,7 @@
-gstreamer = findPackage("gstreamer-0.10", REQUIRED)
-gstreamerApp = findPackage("gstreamer-app-0.10", REQUIRED)
-gstreamerAudio = findPackage("gstreamer-audio-0.10", REQUIRED)
-gstreamerFft = findPackage("gstreamer-fft-0.10", REQUIRED)
+gstreamer = findPackage("gstreamer-1.0", REQUIRED)
+gstreamerApp = findPackage("gstreamer-app-1.0", REQUIRED)
+gstreamerAudio = findPackage("gstreamer-audio-1.0", REQUIRED)
+gstreamerFft = findPackage("gstreamer-fft-1.0", REQUIRED)
 
 audio = Library:new("audio", STATIC)
 audio:addCustomFlags("-std=c++0x")
